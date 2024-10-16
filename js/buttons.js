904850386e511e810e861931fae41ec26fe92a81
giulibrumatti <giulibrumatti97@gmail.com>
2024-10-16 16:34:33 -0300

    finish

--- js/buttons.js
+++ js/buttons.js
@@ -2,9 +2,16 @@ const container1 = document.getElementById('buttons-container1');
 const container2 = document.getElementById('buttons-container2');
 const container3 = document.getElementById('buttons-container3');
 
-for(let i = 9 ; i >= 7 ; i--){
-    const button = document.createElement('button');
-    button.textContent = i;
-    button.id = `button-${i}`
-    container1.appendChild(button);
+function addButtons(desde, hasta, container){
+    for(let i = desde ; i <= hasta ; i++){
+        const button = document.createElement('button');
+        button.textContent = i;
+        button.id = `${i}`
+        button.className = "operator"
+        container.appendChild(button);
+    }
 }
+
+addButtons(7,9,container1);
+addButtons(4,6,container2);
+addButtons(1,3,container3);
